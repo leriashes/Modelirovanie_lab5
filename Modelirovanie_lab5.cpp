ea78ff6cc434e66ddeccc679c3a1c10b62eefb48
leriashes <valeriya-shevchenko-2002@mail.ru>
2023-12-11 16:20:38 +0700

    Чтение файла

--- Modelirovanie_lab5.cpp
+++ Modelirovanie_lab5.cpp
@@ -1,6 +1,54 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ifstream f("input.txt");
+
+	int table[6][6];
+	int index[2][6];
+	int n = 6;
+
+	for (int i = 0; i < n; i++)
+	{
+		index[0][i] = i + 1;
+		index[1][i] = i + 1;
+	}
+
+	//Файл для чтения не открылся
+	if (!f)
+	{
+		cout << "Не удалось открыть файл 'input.txt'";
+		return -1;
+	}
+
+	int i = 0, j = 0;
+
+	//Заполнение таблицы
+	while (!f.eof())
+	{
+		f >> table[i][j];
+		j++;
+
+		if (j == n)
+		{
+			j = 0;
+			i += 1;
+		}
+
+		if (i == n)
+		{
+			break;
+		}
+	}
+
+	f.close();
+
+
+	return 0;
+
 }
