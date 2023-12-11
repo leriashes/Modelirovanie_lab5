1f854e7c5ec1c70c784eab528b7226963121360e
leriashes <valeriya-shevchenko-2002@mail.ru>
2023-12-11 17:52:55 +0700

    Шаги 1 и 2, класс для дерева

--- Modelirovanie_lab5.cpp
+++ Modelirovanie_lab5.cpp
@@ -2,21 +2,103 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <format>
 using namespace std;
 
+int table[6][6];
+int index[2][6];
+int fine[6][6];
+int n = 6;
+
+void printTable()
+{
+	cout << "     ";
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "|   "  << index[0][i] << "  ";
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "\n-----";
+
+		for (int j = 0; j < n; j++)
+		{
+			cout << "-------";
+		}
+
+		cout << "\n  " << index[1][i] << "  ";
+
+		for (int j = 0; j < n; j++)
+		{
+			if (table[i][j] >= 0)
+				cout << std::format("|  {:2}  ", table[i][j]);
+			else
+				cout << "|  inf ";
+		}
+	}
+
+	cout << endl << endl << endl;
+}
+
+void printFineTable()
+{
+	cout << "     ";
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "|   " << index[0][i] << "  ";
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "\n-----";
+
+		for (int j = 0; j < n; j++)
+		{
+			cout << "-------";
+		}
+
+		cout << "\n     ";
+
+		for (int j = 0; j < n; j++)
+		{
+			if (table[i][j] == 0)
+				cout << std::format("| {:<3}  ", fine[i][j]);
+			else
+				cout << "|      ";
+		}
+
+		cout << "\n  " << index[1][i] << "  ";
+
+		for (int j = 0; j < n; j++)
+		{
+			if (table[i][j] >= 0)
+				cout << std::format("|  {:2}  ", table[i][j]);
+			else
+				cout << "|  inf ";
+		}
+	}
+
+	cout << endl << endl << endl;
+}
 
 int main()
 {
-    ifstream f("input.txt");
+	setlocale(LC_ALL, "Rus");
 
-	int table[6][6];
-	int index[2][6];
-	int n = 6;
+    ifstream f("input.txt");
 
 	for (int i = 0; i < n; i++)
 	{
 		index[0][i] = i + 1;
 		index[1][i] = i + 1;
+
+		for (int j = 0; j < n; j++)
+		{
+			fine[i][j] = 0;
+		}
 	}
 
 	//Файл для чтения не открылся
@@ -31,8 +113,7 @@ int main()
 	//Заполнение таблицы
 	while (!f.eof())
 	{
-		f >> table[i][j];
-		j++;
+		f >> table[i][j++];
 
 		if (j == n)
 		{
@@ -49,6 +130,126 @@ int main()
 	f.close();
 
 
-	return 0;
+	cout << "Исходная матрица затрат" << endl << endl;
+	printTable();
 
+	int k = 1;
+
+
+	//шаг 1 - приведение матрицы затрат
+	int r = 0;
+
+	//приведение по строкам
+	for (int i = 0; i < n; i++)
+	{
+		int m = 0;
+
+		for (int j = 0; j < n; j++)
+		{
+			if (table[i][j] >= 0)
+			{
+				m = table[i][j];
+				break;
+			}
+		}
+
+		for (int j = 0; j < n; j++)
+		{
+			if (table[i][j] < m && table[i][j] >= 0)
+			{
+				m = table[i][j];
+			}
+		}
+
+		r += m;
+
+		for (int j = 0; j < n; j++)
+		{
+			table[i][j] -= m;
+		}
+	}
+
+	//приведение по столбцам
+	for (int j = 0; j < n; j++)
+	{
+		int m = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			if (table[i][j] >= 0)
+			{
+				m = table[i][j];
+				break;
+			}
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			if (table[i][j] < m && table[i][j] >= 0)
+			{
+				m = table[i][j];
+			}
+		}
+
+		r += m;
+
+		for (int i = 0; i < n; i++)
+		{
+			table[i][j] -= m;
+		}
+	}
+
+	cout << k << ".1 Приведение матрицы затрат: r = " << r << endl << endl;
+	printTable();
+
+
+	//шаг 2 - вычисление штрафов за неиспользование
+	int maxFine = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (table[i][j] == 0)
+			{
+				int m1 = -1, m2 = -1;
+
+				for (int h = 0; h < n && (m1 < 0 || m2 < 0); h++)
+				{
+					if (table[i][h] >= 0 && h != j)
+					{
+						m1 = table[i][h];
+					}
+
+					if (table[h][j] >= 0 && h != i)
+					{
+						m2 = table[h][j];
+					}
+				}
+
+				for (int h = 0; h < n; h++)
+				{
+					if (table[i][h] < m1 && table[i][h] >= 0 && h != j)
+					{
+						m1 = table[i][h];
+					}
+
+					if (table[h][j] < m2 && table[h][j] >= 0 && h != i)
+					{
+						m2 = table[h][j];
+					}
+				}
+
+				fine[i][j] = m1 + m2;
+
+				if (fine[i][j] > maxFine)
+					maxFine = fine[i][j];
+			}
+		}
+	}
+	
+	cout << k << ".2 Вычисление штрафов: max = " << maxFine << endl << endl;
+	printFineTable();
+
+	return 0;
 }
